b5361ee891f18b9df172ddb1dfa1d305c4eb149c
Raghad Habib <raghadrhabib@gmail.com>
2026-08-29 23:12:13 +0300

    feat: start using pnpm instead of npm and add some eslint rules

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.ts"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+    ],
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/consistent-type-imports": "error",
+      "consistent-return": "error",
+      "no-console": "off",
+      eqeqeq: ["error", "always"],
+    },
+  },
+  eslintConfigPrettier,
+]);
